c5bec9079b0bc5a0f2317d8387ca23280e625e21
Madxine <ki75257525@gmail.com>
2024-10-31 14:59:52 +0100

    list

--- logilogi/app/dashboard/component/list.js
+++ logilogi/app/dashboard/component/list.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+
+const onClick = () => {
+
+};
+
 const List = () => {
   return (
     <div>
-       <ol>
-            <li>Rice</li>
-            <li>Oil</li>
-            <li>...</li>
-        </ol>
+       <ul className='list-none hover:list-disc'>
+            <li>Rice<input/><button>+</button><button>-</button><button>Update</button></li>
+            <li>Oil<input/><button>+</button><button>-</button><button>Update</button></li>
+            <li>...<input/><button>+</button><button>-</button><button>Update</button></li>
+        </ul>
     </div>
   )
 }
--- logilogi/tailwind.config.js
+++ logilogi/tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [    
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/dashboard/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/dashboard/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
